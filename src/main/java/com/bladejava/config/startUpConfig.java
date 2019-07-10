46c09335373cc279dd534f5d2e369b1d5e9bff14
colin <1346536639@qq.com>
2019-07-10 16:42:40 +0800

    修复bug

--- src/main/java/com/bladejava/config/startUpConfig.java
+++ src/main/java/com/bladejava/config/startUpConfig.java
@@ -5,7 +5,7 @@ import com.blade.Blade;
 import com.blade.ioc.annotation.Bean;
 import com.blade.loader.BladeLoader;
 import com.blade.mvc.view.template.JetbrickTemplateEngine;
-import io.github.biezhi.anima.Anima;
+import ralin.Ralin;
 
 @Bean
 public class startUpConfig implements BladeLoader {
@@ -17,7 +17,9 @@ public class startUpConfig implements BladeLoader {
         dataSource.setUrl(blade.environment().getOrNull("jdbc.url"));
         dataSource.setUsername(blade.environment().getOrNull("jdbc.username"));
         dataSource.setPassword(blade.environment().getOrNull("jdbc.password"));
-        Anima.open(dataSource);
-        blade.templateEngine(new JetbrickTemplateEngine());
+        Ralin.open(dataSource);
+
+        //如果你render的页面使用了vue等前端渲染模板，则使用下面的jetbrick模板引擎
+//        blade.templateEngine(new JetbrickTemplateEngine());
     }
 }
